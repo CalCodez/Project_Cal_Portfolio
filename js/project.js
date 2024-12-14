a214a66ddf376f72c33da1dfac274fc155971d72
calvin <CalCodez@outlook.com>
2024-12-14 17:51:31 -0600

    list assignment complete

--- js/project.js
+++ js/project.js
@@ -199,7 +199,7 @@ const panelProjectDescription = getById('panel-project-description');
 const panelProjectLink = getById('panel-project-link');
 
 const [
-	caltubeBtn,
+	calTubeBtn,
 	saasBtn,
 	pokemonBtn,
 	googleBtn,
@@ -246,7 +246,7 @@ const panelProjectsDisplay = (toggler, src, title, description, href) => {
 };
 
 panelProjectsDisplay(
-	caltubeBtn,
+	calTubeBtn,
 	calTube.src,
 	calTube.title,
 	calTube.description,
@@ -303,4 +303,119 @@ panelProjectsDisplay(
 	tribute.title,
 	tribute.description,
 	tribute.href
+);
+
+const projectLiImages = getByClass('project-li-images');
+const projectLiTitles = getByClass('project-li-titleS');
+const projectLiLinks = getByClass('project-li-links');
+
+const [
+	googleImg,
+	saasImg,
+	responsiveImg,
+	calTubeImg,
+	tributeImg,
+	loginsImg,
+	favzImg,
+	pokemonImg,
+] = projectLiImages;
+
+const [
+	googleTitle,
+	saasTitle,
+	responsiveTitle,
+	calTubeTitle,
+	tributeTitle,
+	loginsTitle,
+	favzTitle,
+	pokemonTitle,
+] = projectLiTitles;
+
+const [
+	googleLink,
+	saasLink,
+	responsiveLink,
+	calTubeLink,
+	tributeLink,
+	loginsLink,
+	favzLink,
+	pokemonLink,
+] = projectLiLinks;
+
+const listDisplayAssign = (img, src, title, name, link, href) => {
+	img.src = src;
+	textContent(title, name);
+	link.href = href;
+};
+
+listDisplayAssign(
+	googleImg,
+	google.src,
+	googleTitle,
+	google.title,
+	googleLink,
+	google.href
+);
+
+listDisplayAssign(
+	saasImg,
+	saas.src,
+	saasTitle,
+	saas.title,
+	saasLink,
+	saas.href
+);
+
+listDisplayAssign(
+	responsiveImg,
+	responsive.src,
+	responsiveTitle,
+	responsive.title,
+	responsiveLink,
+	responsive.href
+);
+
+listDisplayAssign(
+	calTubeImg,
+	calTube.src,
+	calTubeTitle,
+	calTube.title,
+	calTubeLink,
+	calTube.href
+);
+
+listDisplayAssign(
+	tributeImg,
+	tribute.src,
+	tributeTitle,
+	tribute.title,
+	tributeLink,
+	tribute.href
+);
+
+listDisplayAssign(
+	loginsImg,
+	logins.src,
+	loginsTitle,
+	logins.title,
+	loginsLink,
+	logins.href
+);
+
+listDisplayAssign(
+	favzImg,
+	favz.src,
+	favzTitle,
+	favz.title,
+	favzLink,
+	favz.href
+);
+
+listDisplayAssign(
+	pokemonImg,
+	pokemon.src,
+	pokemonTitle,
+	pokemon.title,
+	pokemonLink,
+	pokemon.href
 );
